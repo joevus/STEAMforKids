f671b54bbe7499c52d632291b241b7e8c771c1c2
Joe Hos <joehoskisson@gmail.com>
2015-11-11 10:43:27 -0700

    changed header to json on recFromData.php

--- recFromData.php
+++ recFromData.php
@@ -1,6 +1,8 @@
 <?php
 //it worked without this header and with it
-header("Content-Type: text/javascript; charset=utf-8");
+
+header("Content-Type: application/json");
+//header("Content-Type: text/javascript; charset=utf-8");
 //disable caching. To try to get Internet Explorer to update table
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
